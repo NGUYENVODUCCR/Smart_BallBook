ee98cd43cb6a66e450001d5d0dbe894d15a66ed5
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-12-11 22:54:32 +0700

    user model update

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,47 +3,90 @@ import sequelize from "../config/db.js";
 
 class User extends Model {}
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER.UNSIGNED,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
     type: DataTypes.STRING,
     allowNull: true,
+    validate: {
+      isEmail(value) {
+        if (value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+          throw new Error("Email không hợp lệ");
+        }
+      },
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: { isEmail: true },
+
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    role: {
+      type: DataTypes.ENUM("User", "Manager", "Admin"),
+      defaultValue: "User",
+      allowNull: false,
+    },
+
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+
+    google_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    is_verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+
+    gender: {
+      type: DataTypes.ENUM("Nam", "Nữ", "Khác"),
+      allowNull: true,
+    },
+
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    facebook: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true, 
-  },
-  role: {
-    type: DataTypes.ENUM("User", "Manager", "Admin"),
-    defaultValue: "User",
-    allowNull: false,
-  },
-  is_active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-  },
-  google_id: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  is_verified: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-}, {
-  sequelize,
-  modelName: "user",
-  tableName: "users"
-});
+  {
+    sequelize,
+    modelName: "user",
+    tableName: "users",
+    timestamps: true,
+  }
+);
 
 export default User;
