c0e6303ce839a7a3b47d1c73261cdd93a7564ff1
Hieu <ihhghkjihhghkj@gmail.com>
2025-12-12 00:12:27 +0700

    ProtectedRoute.jsx

--- frontend-web/src/components/ProtectedRoute.jsx
+++ frontend-web/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-//
+
 export default function ProtectedRoute({ children, allowedRoles }) {
   const token = localStorage.getItem("token");
   const user = localStorage.getItem("user")
