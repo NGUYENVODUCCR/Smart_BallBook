df69bb243cff27bfc214baa7c415726cf4082104
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-12-11 22:51:11 +0700

    fix cloud

---
+++ backend/src/middlewares/cloudinaryUpload.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "avatars",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+    public_id: (req, file) =>
+      `user-${req.user.id}-${Date.now()}`,
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
